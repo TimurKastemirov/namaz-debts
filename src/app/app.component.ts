314826d00d8392f1b91e676f4659de2822c947b2
Tymur Kastemirov <tamerlan8.05.92@gmail.com>
2020-12-24 10:01:31 +0300

    feat: Add export debts and hide functionality that is in work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { DebtService } from 'src/app/debts/services/debt.service';
+import { Debt } from 'src/app/debts/models/debt';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Namaz debts';
+    title = 'Namaz debts';
+    debts: Debt[];
+
+    constructor(
+        private debtService: DebtService,
+    ) {
+    }
+
+    exportDebts() {
+        this.debtService.getList().subscribe(debts => {
+            const str = JSON.stringify(debts);
+            this.download('debts.json', str);
+        });
+    }
+
+    private download(filename, text) {
+        const element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+
+        element.style.display = 'none';
+        document.body.appendChild(element);
+
+        element.click();
+
+        document.body.removeChild(element);
+    }
 }
